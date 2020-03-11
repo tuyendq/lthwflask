2fe91289f65fda5783aff2aff68eb94fc11a45a3
Tuyen Dang <tuyendq@gmail.com>
2020-03-11 21:45:37 +0700

    config.py: add SQLALCHEMY...

--- microblog/config.py
+++ microblog/config.py
@@ -2,7 +2,7 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATATASE_URL') or \
-        'sqlite:///' + os.path.join(basedir, 'app.db')
+    FLASK_APP = os.environ.get('FLASK_APP')
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'hqapsMev26o4M9W0yF6BMEcPgiAQuvNu'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATATASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
